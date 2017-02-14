a6b97dcc0728d5517d2e872dfd3b45e34c464a5a
kammitama5 <krystal.maughan@gmail.com>
2017-02-13 20:03:39 -0600

    profile python and profile.html file

---
+++ profile.py
@@ -0,0 +1,12 @@
+def __init__(self, first_name=None, last_name=None, 
+                 display_name=None, location=None, email=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.display_name = display_name
+        self.location = location
+        self.email = email
+
+    def print_info(self):
+        print self.display_name, "<" + self.email + ">" 
+
+# Location, Profile Pic -> HTML, Call? Active or Inactive?
