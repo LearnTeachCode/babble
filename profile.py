4dc16f53ed3795d7aa35a54007a07861e57ad4ce
kammitama5 <krystal.maughan@gmail.com>
2017-02-27 20:02:53 -0800

    updated profile.py

--- profile.py
+++ profile.py
@@ -1,19 +1,70 @@
 
 class Profile:
-	def __init__(self, firstname,lastname, username, phonenumber,email):
+	def __init__(self, firstname,lastname, username, email, new_number):
 		self.firstname = firstname 
 		self.lastname = lastname
 		self.username = username
-		self.phonenumber = phonenumber
 		self.email = email
+		self.new_number = new_number
 
-	# user should be able to change username, phone number 
-	def user(self, user1):
-		self.username = user1
+	# firstname -> getters and setters
+	def firstname(self, firstname):
+		self.firstname = firstname
 
-	def phone(self, phone1):
-		self.phonenumber = phone1
+    @property
+    def firstname(self):
+    	print "getter of firstname called"
+    	return self.firstname
 
-    #def print_info(self):
+   	@firstname.set 
+   	def firstname(self, value):
+   		print "setter of firstname called"
+   		self.firstname = value
+
+   	@firstname.deleter
+   	def firstname(self):
+   		print "deleter of firstname called"
+   		del self.firstname
+
+   	# getters and setters -> lastname
+	def lastname(self, lastname):
+		self.lastname = lastname
+
+    @property
+    def lastname(self):
+    	print "getter of lastname called"
+    	return self.lastname
+
+   	@lastname.set 
+   	def lastname(self, value):
+   		print "setter of lastname called"
+   		self.firstname = value
+
+   	@lastname.deleter
+   	def lastname(self):
+   		print "deleter of lastname called"
+   		del self.lastname
+
+   	# getters and setters -> phonenumber
+	def new_number(self, new_number):
+		self.new_number = new_number
+
+    @property
+    def new_number(self):
+    	print "getter of phonenumber called"
+    	return self.new_number
+
+   	@new_number.set 
+   	def new_number(self, value):
+   		print "setter of new_number called"
+   		self.new_number = value
+
+   	@new_number.deleter
+   	def new_number(self):
+   		print "deleter of new_number called"
+   		del self.new_number
+
+
+
+#def print_info(self):
         #print self.username, "<" + self.email + ">" 
-
